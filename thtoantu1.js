923717d24c7f8a2e418a43405e83a6d38def9ec5
Hiimrin <49859850+Hiimrin@users.noreply.github.com>
2019-09-07 21:26:01 +0700

    Add files via upload

---
+++ thtoantu1.js
@@ -0,0 +1,8 @@
+let Width = prompt(parseInt("Nhap vao chieu rong"));
+let Height = prompt(parseInt("Nhap vao chieu cao"));
+let area = Width * Height;
+document.write("Width = " +Width);
+document.write("<br>");
+document.write("Height = " +Height);
+document.write("<br>");
+document.write("The area is: " + area);
